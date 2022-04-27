405eb9deec73a3751db69945f5c51c5fa8d02074
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-27 02:35:30 -0400

    Update jsbm_alert_class.js

--- jsbm_alert_class.js
+++ jsbm_alert_class.js
@@ -1,8 +1,6 @@
 javascript:(
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
-/* jsbm_alert_class.js */
-/* shows an alert message */
 function()
 {
     class OurAlertClass
