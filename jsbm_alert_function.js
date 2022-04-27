bde1b47dbad569137dc6f6bfcd7e3499abd34458
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-26 21:40:57 -0400

    Update jsbm_alert_function.js

--- jsbm_alert_function.js
+++ jsbm_alert_function.js
@@ -1,12 +1,12 @@
 javascript:(
 function()
 {
+    /* Dedicated to God the Father */
+    /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+    
     function showAlert(ourMessage)
     {
         alert(ourMessage);
     }
     showAlert("Howdy");
 })();
-
-/* Dedicated to God the Father */
-/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
